5d0fb2d9f43da22e4e22e28a8483405e4d4e1860
y2ktibltd <75551001+y2ktibltd@users.noreply.github.com>
2021-09-13 15:49:57 -0400

    Update wifi.py

--- wifi.py
+++ wifi.py
@@ -1,6 +1,3 @@
 #!/usr/bin/env python3
 import wifi_qrcode
-
-wifi_qrcode('Network connectivity Problems','WPA',bigham4950,'wifi.png')
-
-
+wifi_qrcode('Network connectivity Problems','WPA',password,'wifi.png')
