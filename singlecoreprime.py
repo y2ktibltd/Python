51c99346f62057333b5074b2f28de575e3b53b80
Tom Bigham <75551001+y2ktibltd@users.noreply.github.com>
2021-09-14 15:41:32 -0400

    Update singlecoreprime.py

--- singlecoreprime.py
+++ singlecoreprime.py
@@ -1,24 +1,21 @@
 #!/usr/bin/env python3
-
 from math import sqrt
 from sys import argv
 import time
-
 start_time=time.time()
-
 try:
     num=int(argv[1])
 except IndexError:
     print("Must type an integer value after command")
     exit() 
-
 def isPrime(num):
     for i in range(2,num):
+        if i%2==0:
+            continue
         for j in range(2,int(sqrt(i))+1):
             if i%j==0:
                 break 
         else:
-            print (i," ")
-
+            print (i,"is a prime")
 isPrime(num)
 print("--- %s seconds ---" % (time.time()-start_time))
