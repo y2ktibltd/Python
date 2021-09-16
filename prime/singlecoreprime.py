a42c11f4f635f8e0ca92e3769941be4a6c2f8daa
Tom Bigham <75551001+y2ktibltd@users.noreply.github.com>
2021-09-16 07:50:53 -0400

    Update singlecoreprime.py
    optimized for odd numbers only by removing if loop to check for division by 2 by starting iteration @ 3 and incrementing by 2 hence skipping even numbers.

--- prime/singlecoreprime.py
+++ prime/singlecoreprime.py
@@ -4,17 +4,15 @@ from sys import argv
 import time
 start_time=time.time()
 try:
-    num=int(argv[1])
+        num=int(argv[1])
 except IndexError:
     print("Must type an integer value after command")
-    exit() 
+    exit()
 def isPrime(num):
-    for i in range(2,num):
-        if i%2==0:
-            continue
-        for j in range(2,int(sqrt(i))+1):
+    for i in range(3,num,2):
+        for j in range(3,int(sqrt(i))+1,2):
             if i%j==0:
-                break 
+                break
         else:
             print (i,"is a prime")
 isPrime(num)
