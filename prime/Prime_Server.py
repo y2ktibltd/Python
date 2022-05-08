9d42de83381bafe5191ec7997c9c9647149fdeb4
Tom Bigham <75551001+y2ktibltd@users.noreply.github.com>
2022-05-08 17:31:50 -0400

    Update and rename Prime_server.py to Prime_Server.py

--- prime/Prime_Server.py
+++ prime/Prime_Server.py
@@ -16,17 +16,18 @@ server=socket.gethostname()
 port=9999
 
 s=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+s.setsockopt(socket.SOL_SOCKET,socket.SO_REUSEADDR,1)
 print(f"Socket {port} Created")
 s.bind((server,port))
 print(f"Bound to {server}")
 s.listen()
 num_list=[_ for _ in range(1,num,2)]
 i=0
-print(num_list)
+#print(num_list)
 
 while i<len(num_list):
     c,addr=s.accept()
-    print(f"Sent {num_list[i]} to {addr}")
+    #print(f"Sent {num_list[i]} to {addr}")
     c.send(str(num_list[i]).encode('ascii'))
     i+=1
     c.close()
