96fbcc01f526e1251d15ac19518a514df77b638e
Tom Bigham <75551001+y2ktibltd@users.noreply.github.com>
2022-05-08 17:18:52 -0400

    Update Prime_Client.py

--- prime/Prime_Client.py
+++ prime/Prime_Client.py
@@ -2,14 +2,20 @@
 from multiprocessing import process, cpu_count
 import socket
 from math import sqrt
+from time import sleep
 
 server="192.168.1.30"
 port=9999
 
-s=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-s.connect((server,port))
-message=s.recv(1024)
-message=int(message)
-s.close()
-print(message)
-print(type(message))
+while True:
+    try:
+        s=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+        s.connect((server,port))
+        message=s.recv(1024)
+        message=int(message)
+        print(message)
+        print(type(message))
+        s.close()
+    except socket.error:
+        print("Done")
+        exit()
