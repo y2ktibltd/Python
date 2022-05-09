021f9d00ca3fbc27b5cdb435d846e2f668e2b689
Tom Bigham <75551001+y2ktibltd@users.noreply.github.com>
2022-05-09 19:31:42 -0400

    Update Prime_Server.py

--- prime/Prime_Server.py
+++ prime/Prime_Server.py
@@ -2,6 +2,7 @@
 import socket
 from sys import argv
 from time import time
+from threading import Thread
 
 try:
             num=int(argv[1])
@@ -22,17 +23,45 @@ s.bind((server,port))
 print(f"Bound to {server}")
 s.listen()
 num_list=[_ for _ in range(1,num,2)]
-i=0
+
+def send_send(num_list):
+    i=0
+    while i<len(num_list):
+        c,addr=s.accept()
+        data=str(num_list[i])
+        buff_len=9-len(data)
+        buff="0"*buff_len
+        send=buff+data
+        c.send(send.encode('ascii'))
+        i+=1
+        c.close()
+
+t1=Thread(target=send_send,args=(num_list,))
+t2=Thread(target=send_send,args=(num_list,))
+t3=Thread(target=send_send,args=(num_list,))
+t4=Thread(target=send_send,args=(num_list,))
+t5=Thread(target=send_send,args=(num_list,))
+t6=Thread(target=send_send,args=(num_list,))
+t7=Thread(target=send_send,args=(num_list,))
+t8=Thread(target=send_send,args=(num_list,))
 start=time()
-while i<len(num_list):
-    c,addr=s.accept()
-    data=str(num_list[i])
-    buff_len=9-len(data)
-    buff="0"*buff_len
-    send=buff+data
-    c.send(send.encode('ascii'))
-    i+=1
-    c.close()
+t1.start()
+t2.start()
+t3.start()
+t4.start()
+t5.start()
+t6.start()
+t7.start()
+t8.start()
+t1.join()
+t2.join()
+t3.join()
+t4.join()
+t5.join()
+t6.join()
+t7.join()
+t8.join()
+
 stop=time()
 total_time=stop-start
 print(f"{total_time} seconds elapsed")
