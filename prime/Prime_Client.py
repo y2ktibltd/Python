ca8abd9b9fd8cd363581d6e0f9b53b8b10e168dc
Tom Bigham <75551001+y2ktibltd@users.noreply.github.com>
2022-05-09 13:01:08 -0400

    Update Prime_Client.py

--- prime/Prime_Client.py
+++ prime/Prime_Client.py
@@ -2,20 +2,27 @@
 from multiprocessing import process, cpu_count
 import socket
 from math import sqrt
-from time import sleep
 
 server="192.168.1.30"
 port=9999
+primes=[]
+
+def isPrime(num):
+    for i in range(3,num,2):
+        for j in range(3,int(sqrt(i)+1),2):
+            if i%j==0:
+                break
+        else:
+            primes.append(i)
 
 while True:
     try:
         s=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
         s.connect((server,port))
-        message=s.recv(1)
-        message=int(message)
-#        print(message)
-#        print(type(message))
+        num=s.recv(128)
+        isPrime(int(num))
         s.close()
     except socket.error:
         print("Done")
+        print(primes)
         exit()
