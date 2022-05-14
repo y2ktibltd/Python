5fe6f9803e28b259edf32f0268067c68a6cc6676
Tom Bigham <75551001+y2ktibltd@users.noreply.github.com>
2022-05-14 19:50:27 -0400

    Update Json_count.py

--- Json/Json_count.py
+++ Json/Json_count.py
@@ -8,7 +8,11 @@ except:
     print(f"File '{argv[1]}' not found")
     exit()
 
+RED = '\033[91m'
+BOLD = '\033[1m'
+END = '\033[0m'
+
 data=json.load(open(argv[1],'r'))
 element_count=len(data)
 print(f"{data}\n")
-print(f"This JSON file contains {element_count} elements")
+print("This JSON file contains "+RED+BOLD+f"{element_count}"+END+" elements")
