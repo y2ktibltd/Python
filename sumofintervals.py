46340bd8bd0c2f68f1ac786c62637a06e209ad31
Tom Bigham <75551001+y2ktibltd@users.noreply.github.com>
2022-09-03 07:48:44 -0400

    Update sumofintervals.py

--- sumofintervals.py
+++ sumofintervals.py
@@ -16,7 +16,7 @@ print(sum_of_intervals([(1, 5),(1,5)])) # 4
 print(sum_of_intervals([(1, 5), (6, 10)])) # 8
 print(sum_of_intervals([(1, 5), (1, 5)])) # 4
 print(sum_of_intervals([(1, 4), (7, 10), (3, 5)])) # 7
-#rint(sum_of_intervals([(1,10),(-100_000_001,100_000_000)])) # 2_000_000_000
+#print(sum_of_intervals([(1,10),(-100_000_001,100_000_000)])) # 2_000_000_000
 #print(sum_of_intervals([(-1_000_000_000, 1_000_000_000)])) # 2_000_000_000
 #print(sum_of_intervals([(0, 20), (-100_000_000, 10), (30, 40)])) # 100_000_030
-#rint(sum_of_intervals([[randint(randint(-1_000_000_000,0),1),randint(1,randint(1,1_000_000_000))] for i in range(randint(1,10))]))
+#print(sum_of_intervals([[randint(randint(-1_000_000_000,0),1),randint(1,randint(1,1_000_000_000))] for i in range(randint(1,10))]))
